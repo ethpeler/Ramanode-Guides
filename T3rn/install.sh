542b43864525cacb2539845a46c96362b1b3f891
laynuril <141418259+ethpeler@users.noreply.github.com>
2024-10-10 22:40:41 +0700

    Update install.sh

--- T3rn/install.sh
+++ T3rn/install.sh
@@ -13,7 +13,7 @@ sudo apt -q update
 sudo apt -qy upgrade
 
 EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.6/executor-linux-v0.21.6.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.21.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.21.6.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
